eb482698936510460fe824f7f1cacbdc474c2e7a
Thanos Dimitriades <78486373+thanosades@users.noreply.github.com>
2021-02-06 15:15:41 +0200

    functional game

--- src/index.js
+++ src/index.js
@@ -22,11 +22,16 @@ function Square(props) {
 
 function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(i) {
+    if (squares[i] || winner) {
+      return;
+    }
     const newSquares = squares.slice();
-    newSquares[i] = 'X';
+    newSquares[i] = xIsNext ? 'X' : 'O';
     setSquares(newSquares);
+    setXIsNext(!xIsNext);
   }
 
   function renderSquare(i) {
@@ -38,7 +43,14 @@ function Board() {
     );
   }
 
-  const status = 'Next player: X';
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext? 'X' : 'O'}`;
+  }
 
   return (
     <div>
@@ -82,3 +94,24 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
